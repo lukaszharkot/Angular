4e20f678a2d6b9a163b8d198ff64a9ac6e292f16
lukaszharkot <lukasz.harkot22@gmail.com>
2024-10-09 01:06:50 +0200

    first commit

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'id-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
